7db0f7a9996f23f3110fada66d06820d25b29839
LivSterling <sterlingchapmanmade@gmail.com>
2025-03-23 23:53:09 -0400

    more

--- js/main.js
+++ js/main.js
@@ -1,129 +1,226 @@
 // Goal: Create a two player Tic-Tac-Toe game. The users should be able to click to place their X or O and if they win the program should mention their win in the DOM. Please make the game as OOP as possible.
-const grid = {
-gridOne: document.getElementById('gOne'),
-gridTwo: document.getElementById('gTwo'),
-gridThree: document.getElementById('gThree'),
-gridFour: document.getElementById('gFour'),
-gridFive: document.getElementById('gFive'),
-gridSix: document.getElementById('gSix'),
-gridSeven: document.getElementById('gSeven'),
-gridEight: document.getElementById('gEight'),
-gridNine: document.getElementById('gNine'),
-}
+// const grid = {
+//     gridOne: document.getElementById('gOne'),
+//     gridTwo: document.getElementById('gTwo'),
+//     gridThree: document.getElementById('gThree'),
+//     gridFour: document.getElementById('gFour'),
+//     gridFive: document.getElementById('gFive'),
+//     gridSix: document.getElementById('gSix'),
+//     gridSeven: document.getElementById('gSeven'),
+//     gridEight: document.getElementById('gEight'),
+//     gridNine: document.getElementById('gNine'),
+// }
 // const rowOne = document.getElementsByClassName('r1')
 let isO = false
+const confetti = document.getElementById('congratulations') 
+const restart = document.getElementById('restart')
 
-
-const winLines = {
-    rowOne: document.getElementsByClassName('r1'),
-    rowTwo: document.getElementsByClassName('r2'),
-    rowThree: document.getElementsByClassName('r3'),
-    columnOne: document.getElementsByClassName('c1'),
-    columnTwo: document.getElementsByClassName('c2'),
-    columnThree: document.getElementsByClassName('c3'),
-    diagonalOne: document.getElementsByClassName('d1'),
-    diagonalTwo: document.getElementsByClassName('d2')
-}
-
-// function checkForWin() {
-//     if (gridOne.innerHTML === "X" && gridTwo.innerHTML === "X" && gridThree.innerHTML === "X" || grid) {
-//         console.log("X wins")
-//     }
-        
-// }
-
-function checkForWin(className) {
-    const elements = className;
+const winLines = [
+    rowOne = document.getElementsByClassName('r1'),
+    rowTwo = document.getElementsByClassName('r2'),
+    rowThree = document.getElementsByClassName('r3'),
+    columnOne = document.getElementsByClassName('c1'),
+    columnTwo = document.getElementsByClassName('c2'),
+    columnThree = document.getElementsByClassName('c3'),
+    diagonalOne =document.getElementsByClassName('d1'),
+    diagonalTwo =document.getElementsByClassName('d2'),
+]
+document.getElementById('playerX').innerHTML = 0
+document.getElementById('playerO').innerHTML = 0
+ function checkForWin() {
+    winLines.forEach(property => {
+    const elements = property;
 
     const firstInnerHTML = elements[0].innerHTML;
     for (let i = 1; i < elements.length; i++) {
-      if (elements[i].innerHTML !== firstInnerHTML) {
+      if (elements[i].innerHTML !== firstInnerHTML || elements[i].innerHTML === "") {
         return false;
       } 
     } if (firstInnerHTML === 'X') {
         console.log("X wins")
+        document.getElementById('playerX').innerHTML ++
+        confetti.classList.remove('hidden')
     } else {
         console.log("O wins")
+        document.getElementById('playerO').innerHTML ++
+        confetti.classList.remove('hidden')
     }
+  })
+}
 
-  }
+const boxes = document.querySelectorAll('.box');
 
-grid.gridOne.addEventListener('click', () => {
+boxes.forEach(element => {
+    element.addEventListener('click', () => {
+    console.log(element);
+    if (element.innerHTML === "") {
     if (isO === false) {
-        gridOne.innerHTML = "X"
+        element.innerHTML = "X"
     } else {
-        gridOne.innerHTML = "O"
+        element.innerHTML = "O"
     }
+}
     isO = !isO
-    checkForWin(winLines.rowOne )
-    checkForWin(winLines.columnTwo)
-    checkForWin(winLines.diagonalOne)
+    checkForWin()
 })
-gridTwo.addEventListener('click', () => {
-    if (isO === false) {
-        gridTwo.innerHTML = "X"
-    } else {
-        gridTwo.innerHTML = "O"
-    }
-    isO = !isO
+})
 
+restart.addEventListener('click', () => {
+    boxes.forEach(element => {
+        element.innerHTML = ""
+        confetti.classList.add('hidden')
+    })
 })
-gridThree.addEventListener('click', () => {
-    if (isO === false) {
-        gridThree.innerHTML = "X"
-    } else {
-        gridThree.innerHTML = "O"
-    }
-    isO = !isO
-})
-gridFour.addEventListener('click', () => {
-    if (isO === false) {
-        gridFour.innerHTML = "X"
-    } else {
-        gridFour.innerHTML = "O"
-    }
-    isO = !isO
-})
-gridFive.addEventListener('click', () => {
-        if (isO === false) {
-        gridFive.innerHTML = "X"
-    } else {
-        gridFive.innerHTML = "O"
-    }
-    isO = !isO
-})
-gridSix.addEventListener('click', () => {
-        if (isO === false) {
-        gridSix.innerHTML = "X"
-    } else {
-        gridSix.innerHTML = "O"
-    }
-    isO = !isO
-})
-gridSeven.addEventListener('click', () => {
-        if (isO === false) {
-        gridSeven.innerHTML = "X"
-    } else {
-        gridSeven.innerHTML = "O"
-    }
-    isO = !isO
-})
-gridEight.addEventListener('click', () => {
-        if (isO === false) {
-        gridEight.innerHTML = "X"
-    } else {
-        gridEight.innerHTML = "O"
-    }
-    isO = !isO
-})
-gridNine.addEventListener('click', () => {
-        if (isO === false) {
-        gridNine.innerHTML = "X"
-    } else {
-        gridNine.innerHTML = "O"
-    }
-    isO = !isO
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// grid.gridOne.addEventListener('click', () => {
+//     if (isO === false) {
+//         grid.gridOne.innerHTML = "X"
+//     } else {
+//         grid.gridOne.innerHTML = "O"
+//     }
+//     isO = !isO
+//     winLines.checkForWin(winLines.rowOne )
+//     checkForWin(winLines.columnTwo)
+//     checkForWin(winLines.diagonalOne)
+// })
+// grid.gridTwo.addEventListener('click', () => {
+//     if (isO === false) {
+//         gridTwo.innerHTML = "X"
+//     } else {
+//         gridTwo.innerHTML = "O"
+//     }
+//     isO = !isO
+
+// })
+// gridThree.addEventListener('click', () => {
+//     if (isO === false) {
+//         gridThree.innerHTML = "X"
+//     } else {
+//         gridThree.innerHTML = "O"
+//     }
+//     isO = !isO
+// })
+// gridFour.addEventListener('click', () => {
+//     if (isO === false) {
+//         gridFour.innerHTML = "X"
+//     } else {
+//         gridFour.innerHTML = "O"
+//     }
+//     isO = !isO
+// })
+// gridFive.addEventListener('click', () => {
+//         if (isO === false) {
+//         gridFive.innerHTML = "X"
+//     } else {
+//         gridFive.innerHTML = "O"
+//     }
+//     isO = !isO
+// })
+// gridSix.addEventListener('click', () => {
+//         if (isO === false) {
+//         gridSix.innerHTML = "X"
+//     } else {
+//         gridSix.innerHTML = "O"
+//     }
+//     isO = !isO
+// })
+// gridSeven.addEventListener('click', () => {
+//         if (isO === false) {
+//         gridSeven.innerHTML = "X"
+//     } else {
+//         gridSeven.innerHTML = "O"
+//     }
+//     isO = !isO
+// })
+// gridEight.addEventListener('click', () => {
+//         if (isO === false) {
+//         gridEight.innerHTML = "X"
+//     } else {
+//         gridEight.innerHTML = "O"
+//     }
+//     isO = !isO
+// })
+// gridNine.addEventListener('click', () => {
+//         if (isO === false) {
+//         gridNine.innerHTML = "X"
+//     } else {
+//         gridNine.innerHTML = "O"
+//     }
+//     isO = !isO
+// })
 
 
 //first click id always an x sencond is O ect.
